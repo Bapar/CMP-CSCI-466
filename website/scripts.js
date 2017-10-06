f28af93a89e8f788e7bfa069a26ca335f3973498
Bapar <bap_91@msn.com>
2017-10-06 16:27:21 -0600

    updated player hand, added getImage function -bp

--- website/scripts.js
+++ website/scripts.js
@@ -6,12 +6,19 @@ jQuery(function ($) {
 
 //initial setup
 $(document).ready(function(){
+	var game = createGame();
 	var player = createPlayer();
 	$( "#endTurnButton" ).click(function() {
 		endTurn(player);
 	});
 	
 }); 
+
+function createGame() {
+	var game = {
+		supply: []
+	};
+}
 
 function createPlayer(){
 
@@ -30,9 +37,20 @@ function createPlayer(){
 	shuffle(player.deck);
 	//draw 5 new cards
 	draw(player, 5);
+
+	renderHand(player);
+
 	return player;
 }
 
+// Render the players hand on screen
+function renderHand(player) {
+	for (i = 0; i < 5; i++){
+		handNum = "hand" + i;
+		document.getElementById(handNum).style.backgroundImage = getImage(player.hand[i]);
+		//alert(player.hand[0]);
+	}
+}
 
 //Fisher-Yates suffling algorith from
 //https://bost.ocks.org/mike/shuffle/
@@ -86,4 +104,66 @@ function endTurn(player){
 //	console.log("discard: " + player.discardPile);
 	
 	return player;
+}
+
+//--------------------------------------------------------
+function getImage(card) {
+	 switch (card) {
+         case "copper":
+             return "url('assets/cards/200px-Copper.jpg')";
+             break;
+         case "silver":
+             return "url('assets/cards/200px-Silver.jpg')";
+             break;
+         case "gold":
+             return "url('assets/cards/200px-Gold.jpg')";
+             break;
+         case "estate":
+             return "url('assets/cards/200px-Estate.jpg')";
+             break;
+		 case "duchy":
+             return "url('assets/cards/200px-Duchy.jpg')";
+             break;
+         case "province":
+             return "url('assets/cards/200px-Province.jpg')";
+             break;
+         case "chapel":
+             return "url('assets/cards/200px-Chapel.jpg')";
+             break;
+         case "council room":
+             return "url('assets/cards/200px-Council_Room.jpg')";
+             break;
+         case "festival":
+             return "url('assets/cards/200px-Festival.jpg')";
+             break;
+         case "gardens":
+             return "url('assets/cards/200px-Gardens.jpg')";
+             break;
+         case "laboratory":
+             return "url('assets/cards/200px-Laboratory.jpg')";
+             break;
+         case "market":
+             return "url('assets/cards/200px-Market.jpg')";
+             break;
+		 case "village":
+             return "url('assets/cards/200px-Village.jpg')";
+             break;
+         case "witch":
+             return "url('assets/cards/200px-Witch.jpg')";
+             break;
+         case "woodcutter":
+             return "url('assets/cards/200px-Woodcutter.jpg')";
+             break;
+         case "workshop":
+             return "url('assets/cards/200px-Workshop.jpg')";
+             break;
+         case "curse":
+             return "url('assets/cards/200px-Curse.jpg')";
+             break;
+         case "trash":
+             return "url('assets/cards/200px-Trash-new.jpg')";
+             break;
+         default:
+         	return "url('assets/cards/200px-Cardback.jpg')";
+	 }
 }
